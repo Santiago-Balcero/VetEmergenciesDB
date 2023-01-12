0896a2817da182b4c2f56b01ceb3c97055658f88
DAVID SANTIAGO BALCERO CERON <1032432026@bolivar.com>
2023-01-12 11:21:46 -0500

    Insert scripts for DB data

---
+++ sql/CLINIC_202301121118.sql
@@ -0,0 +1,40 @@
+INSERT INTO APP_GEST_E_MASCOTAS.CLINIC (ID_CLINIC,CLINIC_NAME,CLINIC_NIT,ID_DEPARTMENT,CLINIC_CITY,CLINIC_ADDRESS,CLINIC_LATITUDE,CLINIC_LONGITUDE,CLINIC_TELEPHONE,CLINIC_EMAIL,IS_ACTIVE,LAST_UPDATE_DATE,LAST_UPDATE_ACTION) VALUES
+	 (82,'Pet Lovers','111111112',5,'Bogotá D.C.','Calle 17 100-50',4.673376699999999,-74.146281,'6015555555','petlovers@mail.com',1,TIMESTAMP'2022-12-20 11:15:54.0','Creation'),
+	 (83,'The Jungle','111111113',1,'Medellín','Calle 46 54-11',6.2482957,-75.5726531,'6048559963','jungle@vet.com',1,TIMESTAMP'2022-12-20 11:21:26.0','Data update'),
+	 (86,'Cachorros Veterinaria','111111115',31,'Cali','Calle 22 20-20',3.457975,-76.52452219999999,'6058888888','cachorros@mail.com',1,TIMESTAMP'2022-12-20 11:24:09.0','Creation'),
+	 (89,'Tiburoncín Vet','111111118',4,'Barranquilla','Calle 12 12-12',11.0041072,-74.80698129999999,'6095555666','tiburon@mail.com',1,TIMESTAMP'2022-12-20 11:26:31.0','Creation'),
+	 (90,'Sin pulgas vet','222222221',7,'Tunja','Calle 15 60-99',5.5446422,-73.3575572,'6015555555','pulgas@mail.com',1,TIMESTAMP'2022-12-20 11:27:23.0','Creation'),
+	 (92,'Brisa del mar veterinaria','222222224',6,'Cartagena','Calle 17 10-50',10.3932277,-75.4832311,'6058888888','mar@mail.com',1,TIMESTAMP'2022-12-20 11:28:47.0','Creation'),
+	 (93,'AgroVet Clínica','222222225',7,'Tunja','Calle 46 54-11',5.5583654,-73.357215,'6048559963','agro@mail.com',1,TIMESTAMP'2022-12-20 11:29:26.0','Creation'),
+	 (115,'Techcamp Veterinaria','123521001',5,'Bogotá D.C.','Calle 45 24-20',4.6332001,-74.07263069999999,'6015555555','tech@mail.com',1,TIMESTAMP'2022-12-26 11:21:57.0','Creation'),
+	 (85,'Best Friends Vet','111111114',1,'Medellín','Calle 10 5-5',6.2196998,-75.59841689999999,'6048559963','friends@mail.com',1,TIMESTAMP'2022-12-20 11:23:05.0','Creation'),
+	 (41,'Peludos Vet','999999999',5,'Bogotá D.C.','Carrera 6 15-88',4.6017872,-74.0721736,'6010000000','patitas@mail.com',0,TIMESTAMP'2022-12-25 22:44:07.0','Deactivate');
+INSERT INTO APP_GEST_E_MASCOTAS.CLINIC (ID_CLINIC,CLINIC_NAME,CLINIC_NIT,ID_DEPARTMENT,CLINIC_CITY,CLINIC_ADDRESS,CLINIC_LATITUDE,CLINIC_LONGITUDE,CLINIC_TELEPHONE,CLINIC_EMAIL,IS_ACTIVE,LAST_UPDATE_DATE,LAST_UPDATE_ACTION) VALUES
+	 (42,'Clínica Primavera','222222222',5,'Bogotá D.C.','Carrera 44 13f-4',4.6244214,-74.1026973,'6010000000','primavera@mail.com',1,TIMESTAMP'2022-12-13 18:32:04.0','Data update'),
+	 (87,'La granja clínica veterinaria','111111116',31,'Cali','Carrera 30 40-10',3.421348,-76.5015774,'6058888888','granja@granja.com',1,TIMESTAMP'2022-12-20 11:25:06.0','Creation'),
+	 (88,'El huerto veterinaria','111111117',4,'Barranquilla','Calle 15 60-99',10.9597113,-74.7736966,'6095555666','huerto@mail.com',1,TIMESTAMP'2022-12-20 11:25:46.0','Creation'),
+	 (91,'Mascota Feliz','222222223',20,'Santa Marta','Calle 10 5-5',11.2403547,-74.2110227,'6015555555','feliz@mail.com',1,TIMESTAMP'2022-12-20 11:28:02.0','Creation'),
+	 (94,'Veterinaria del Valle','222222227',31,'Cali','Carrera 17c 46-00',3.4397421,-76.5216358,'6095555666','delvalle@mail.com',1,TIMESTAMP'2022-12-20 11:30:30.0','Creation'),
+	 (95,'La selva veterinaria','222222229',15,'Girardot','Calle 15 20-99',4.3045959,-74.8031414,'6015555555','selva@mail.com',1,TIMESTAMP'2022-12-20 11:32:26.0','Creation'),
+	 (96,'Real Mascotas','333333339',28,'Bucaramanga','Calle 10 5-5',7.0784489,-73.1339066,'6058888888','realm@mail.com',1,TIMESTAMP'2022-12-20 11:33:39.0','Creation'),
+	 (97,'Veterinaria María','777777774',30,'Ibagué','Calle 10 5-5',4.4460891,-75.24143339999999,'6015555555','mar@vetmar.com',1,TIMESTAMP'2022-12-20 11:35:08.0','Creation'),
+	 (98,'San Perro Clínica Veterinaria','777777776',8,'Manizales','Calle 22 20-20',5.068882599999999,-75.5177985,'6095555666','sanperro@mail.com',1,TIMESTAMP'2022-12-20 11:35:50.0','Creation'),
+	 (110,'Clínica Veterinaria Bolívar','125845263',5,'Bogotá D.C.','Avenida Calle 26 68b-31',4.655509100000001,-74.1056894,'6015555555','bolivarveterinaria@mail.com',1,TIMESTAMP'2022-12-25 21:42:55.0','Activate');
+INSERT INTO APP_GEST_E_MASCOTAS.CLINIC (ID_CLINIC,CLINIC_NAME,CLINIC_NIT,ID_DEPARTMENT,CLINIC_CITY,CLINIC_ADDRESS,CLINIC_LATITUDE,CLINIC_LONGITUDE,CLINIC_TELEPHONE,CLINIC_EMAIL,IS_ACTIVE,LAST_UPDATE_DATE,LAST_UPDATE_ACTION) VALUES
+	 (112,'Veterinaria Perritos Bolívar','125895421',5,'Bogotá D.C.','Calle 19 4-5',4.6035694,-74.06891809999999,'6015555554','perritosbolivar@mail.com',1,TIMESTAMP'2022-12-25 22:48:17.0','Data update'),
+	 (71,'ZooAmigos','123320252',15,'Sibaté','Calle 10 8-01',4.491196,-74.2593771,'6010000000','zooamigos@mail.com',1,TIMESTAMP'2022-12-15 14:48:53.0','Creation'),
+	 (66,'Los Michis Vet','444444444',5,'Bogotá D.C.','Carrera 58 46-36',4.6503085,-74.0916462,'6013333339','michis@mail.com',1,TIMESTAMP'2022-12-12 11:30:30.0','Data update'),
+	 (72,'EmerVet Clínica Veterinaria','230021001',15,'Funza','Carrera 3 18-58',4.7124288,-74.22030939999999,'6013333333','emervet@mail.com',1,TIMESTAMP'2022-12-15 14:51:24.0','Creation'),
+	 (81,'Veterinaria Mauro','120023302',15,'Mosquera','Calle 10 5-5',4.7129366,-74.2262404,'6015555555','mauro@mail.com',1,TIMESTAMP'2022-12-19 11:45:44.0','Data update'),
+	 (108,'Zerob Vet','526325412',5,'Bogotá D.C.','Carrera 13 64-20',4.6508921,-74.063159,'6015555555','zerob@mail.com',1,TIMESTAMP'2022-12-23 16:56:47.0','Creation'),
+	 (43,'Lomitos Veterinaria','333333333',15,'Cota','Calle 14 2b-27',4.8099598,-74.0979922,'6010000000','lomitos@mail.com',1,TIMESTAMP'2022-12-10 11:04:49.0','Creation'),
+	 (22,'Calypsa Vet','111111111',5,'Bogotá D.C.','Calle 59 14a-82',4.6463755,-74.0669826,'6010000007','calypsa@mail.com',1,TIMESTAMP'2022-12-23 17:10:44.0','Data update'),
+	 (46,'Rock and Dogs','555555555',5,'Bogotá D.C.','Calle 53 17-12',4.641062499999999,-74.0693714,'6010000000','rockanddogs@mail.com',1,TIMESTAMP'2022-12-12 10:32:44.0','Data update'),
+	 (48,'VitaVet','666666666',5,'Bogotá D.C.','Carrera 20 76-20',4.7425982,-74.04409,'6013333333','vitavet@mail.com',1,TIMESTAMP'2022-12-10 14:53:36.0','Creation');
+INSERT INTO APP_GEST_E_MASCOTAS.CLINIC (ID_CLINIC,CLINIC_NAME,CLINIC_NIT,ID_DEPARTMENT,CLINIC_CITY,CLINIC_ADDRESS,CLINIC_LATITUDE,CLINIC_LONGITUDE,CLINIC_TELEPHONE,CLINIC_EMAIL,IS_ACTIVE,LAST_UPDATE_DATE,LAST_UPDATE_ACTION) VALUES
+	 (49,'Patitas Veterinaria','777777777',5,'Bogotá D.C.','Calle 45 24-10',4.5792772,-74.1266344,'6013333333','patitas@mail.com',0,TIMESTAMP'2022-12-26 11:16:02.0','Deactivate'),
+	 (68,'Tom & Jerry Veterinaria','777774458',15,'Funza','Calle 19 6-10',4.6048681,-74.0708245,'6013333333','tomjerry@mail.com',1,TIMESTAMP'2022-12-23 12:07:08.0','Deactivate'),
+	 (69,'Pet Grooming Spa','999563254',15,'Sibaté','Calle 5 4-74',4.491196,-74.2593771,'6013333333','spa@mail.com',1,TIMESTAMP'2022-12-13 12:24:09.0','Creation'),
+	 (70,'Pájaros Vet','999666585',5,'Bogotá D.C.','Calle 140 19-30',4.7236255,-74.0465746,'6013333333','pajaros@mail.com',1,TIMESTAMP'2022-12-13 13:31:23.0','Creation'),
+	 (107,'San Can Veterinaria','956636999',5,'Bogotá D.C.','Calle 127 100-20',4.7264189,-74.0996054,'6015555555','sancanvet@mail.com',1,TIMESTAMP'2022-12-23 11:38:32.0','Creation'),
+	 (116,'Clínica Eduardo Vet','125412563',5,'Bogotá D.C.','Carrera 50 63-70',4.6689789,-74.0737685,'6015555555','eduardo@mail.com',1,TIMESTAMP'2022-12-26 11:53:45.0','Creation');
